c0af2417c2fcb97ca40724984937e28645ef8e6f
boski <boskitechh@gmail.com>
2022-04-27 17:24:08 +0100

    Made ajax calls

--- src/features/posts/Posts.js
+++ src/features/posts/Posts.js
@@ -1,98 +1,124 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { nanoid } from "@reduxjs/toolkit"
-import { postAdded, deletePost } from "./postSlice"
+import { postAdded, deletePost, postUpdate, fetchPosts, selectAllPosts } from "./postSlice"
 
 
 const Posts = () => {
-  const posts = useSelector(state => state.posts)
+  const posts = useSelector(selectAllPosts)
+  const postStatus = useSelector(state => state.posts.status)
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
-
+  const [btnName, setBtnName] = useState('Add Post')
+  const [id, setId] = useState('')
   const dispatch = useDispatch()
 
+  useEffect(() => {
+    if (postStatus === 'idle') {
+      dispatch(fetchPosts())
+      console.log(posts)
+    }
+  }, [postStatus, posts, dispatch])
 
   const savePost = () => {
-    if(title && content){
+    if (title && content && btnName === 'Add Post') {
       dispatch(
-        postAdded({id: nanoid(), title, content})
+        postAdded({ id: nanoid(), title, content })
       )
 
       setTitle('')
       setContent('')
+    } else {
+      dispatch(
+        postUpdate({ id, title, content })
+      )
+
+      setTitle('')
+      setContent('')
+      setBtnName('Add Post')
     }
   }
 
+  const editPost = (post) => {
+    setId(post.id)
+    setTitle(post.title)
+    setContent(post.content)
+    setBtnName('Update Post')
+  }
+
   return (
-    <div style={{width: '500px', height:'650px', margin:'100px auto', backgroundColor:'#888888', padding:'0px'}} className="Counter">
-      <div style={{height:'40px', backgroundColor:'#333333', padding:'15px', color:'#fff', fontSize:'30px'}} className="Header">
+    <div style={{ width: '500px', height: '650px', margin: '100px auto', backgroundColor: '#cccccc', padding: '0px' }} className="Counter">
+      <div style={{ height: '40px', backgroundColor: '#333333', padding: '15px', color: '#fff', fontSize: '30px' }} className="Header">
         <b>Redux Post App</b>
       </div>
       <div className="postDiv">
-        <div style={{marginTop:'20px'}} className='textfields'>
+        <div style={{ marginTop: '20px' }} className='textfields'>
           <input type="text" placeholder='Title'
-          value={title}
-          onChange={e => setTitle(e.target.value)}
-          style={{
-            border:'none',
-            height:'50px',
-            width:'400px',
-            margin:'10px 45px',
-            fontSize:'20px',
-            paddingLeft:'7px'
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            style={{
+              border: 'none',
+              height: '50px',
+              width: '400px',
+              margin: '10px 45px',
+              fontSize: '20px',
+              paddingLeft: '7px'
 
-          }}/>
+            }} />
           <input type="text" placeholder='Content'
-          value={content}
-          onChange={e => setContent(e.target.value)}
-          style={{
-            border:'none',
-            height:'50px',
-            width:'400px',
-            margin:'0px 45px',
-            fontSize:'20px',
-            paddingLeft:'7px'
-          }}/>
-          <button className="btn-minus" 
-          onClick={() => savePost()}
-          style={{
-            width:'410px', 
-            height:'47px', 
-            fontSize:'25px', 
-            border:'none', 
-            backgroundColor:'#333333',
-            margin:'20px 45px 30px 45px',
-            color:'white',
-            cursor:'pointer'
-          }}>
-            <b>Submit</b>
+            value={content}
+            onChange={e => setContent(e.target.value)}
+            style={{
+              border: 'none',
+              height: '50px',
+              width: '400px',
+              margin: '0px 45px',
+              fontSize: '20px',
+              paddingLeft: '7px'
+            }} />
+          <button className="btn-minus"
+            onClick={() => savePost()}
+            style={{
+              width: '410px',
+              height: '47px',
+              fontSize: '25px',
+              border: 'none',
+              backgroundColor: '#333333',
+              margin: '20px 45px 30px 45px',
+              color: 'white',
+              cursor: 'pointer'
+            }}>
+            <b>{btnName}</b>
           </button>
         </div>
       </div>
-      <div style={{borderTop:'1px solid #cccccc', padding:'20px auto', height:'330px', overflow: 'auto'}} className="display">
-          {posts.map(post => (
-            <div  key={post.id} style={{
-              width:'420px',
-              margin:'10px auto',
-              height:'auto',
-              border:'1px solid #hhhhhh',
-              borderRadius:'3px',
-              backgroundColor: '#cccccc',
-              padding:'10px',
-              color: '#333333'
+      <div style={{ borderTop: '1px solid #eeeeee', padding: '20px auto', height: '330px', overflow: 'auto' }} className="display">
+        {posts.map(post => (
+          <div key={post.id}
+            onClick={() => editPost(post)}
+            style={{
+              width: '420px',
+              margin: '10px auto',
+              height: 'auto',
+              border: '1px solid #hhhhhh',
+              borderRadius: '3px',
+              backgroundColor: '#aaaaaa',
+              padding: '10px',
+              color: '#333333',
+              cursor: 'pointer'
             }}>
-              
-              <span style={{fontSize:'25px'}}>{post.title}
-                <span onClick={() => dispatch(deletePost(post.id))} style={{fontSize:'20px', cursor:'pointer', float: 'right', color:'red'}}>
-                  <b>X</b>
-                </span>
+
+            <span style={{ fontSize: '25px' }}>{post.title}
+              <span onClick={() => dispatch(deletePost(post.id))} style={{ fontSize: '20px', cursor: 'pointer', float: 'right', color: 'red' }}>
+                <b>X</b>
               </span>
-              <br/>
-              <p style={{fontSize:'15px', marginTop:'7px'}}>{post.content}</p>
-  
-            </div>
-          ))}
-          
+            </span>
+            <br />
+            <p style={{ fontSize: '15px', marginTop: '7px' }}>{post.content}</p>
+
+          </div>
+        ))}
+
       </div>
 
     </div>
--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -1,21 +1,63 @@
-import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-const initialState = [
-    { id: '1', title: 'First Post!', content: 'Hello!' },
-    { id: '2', title: 'Second Post', content: 'More text' }
-  ]
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+    const response = await axios.get('http://localhost:5000/posts');
+    return response.data;
+})
+
+export const addPost = createAsyncThunk('posts/addNewPost', async initialPost => {
+    const response = await axios.post('http://localhost:5000/posts', initialPost)
+    return response.data
+})
+
+const initialState = {
+    posts: [],
+    status: 'idle',
+    error: null
+}
 
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
-    reducers:{
-        postAdded:(state, action) => {
-            state.push(action.payload)
+    reducers: {
+        postAdded: (state, action) => {
+            state.posts.push(action.payload)
         },
-        deletePost:(state, action) => state.filter((post) => post.id !== action.payload)
+        deletePost: (state, action) => state.posts.filter((post) => post.id !== action.payload),
+        postUpdate: (state, action) => {
+            const { id, title, content } = action.payload
+            const existingPost = state.posts.find((post) => post.id === id)
+            if (existingPost) {
+                existingPost.title = title
+                existingPost.content = content
+            }
+        }
+    },
+    extraReducers(builder) {
+        builder
+            .addCase(fetchPosts.pending, (state, action) => {
+                state.status = 'loading'
+            })
+            .addCase(fetchPosts.fulfilled, (state, action) => {
+                state.status = 'succeeded'
+                // Add any fetched posts to the array
+                state.posts = action.payload
+            })
+            .addCase(fetchPosts.rejected, (state, action) => {
+                state.status = 'failed'
+                state.error = action.error.message
+            })
+            .addCase(addPost.fulfilled, (state, action) => {
+                state.posts.push(action.payload)
+            })
     }
 })
 
-export const { postAdded, deletePost } = postsSlice.actions
+export const { postAdded, deletePost, postUpdate } = postsSlice.actions
+
+export const selectAllPosts = state => state.posts.posts
+
+export const selectPostBtId = (state, postId) => state.posts.posts.find(post => post.id === postId)
 
 export default postsSlice.reducer
